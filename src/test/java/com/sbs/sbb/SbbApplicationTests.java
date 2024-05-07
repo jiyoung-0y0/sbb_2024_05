7bf3d6a546edc02ed231f4fa2f1ba98dc7433faa
jiyoung-OyO <kimwldud1234@gmail.com>
2024-05-07 16:35:44 +0900

    데이터삭제 @Transaction, @Rollback

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -1,14 +1,18 @@
 package com.sbs.sbb;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Transactional
+//@Rollback(true)
 class SbbApplicationTests {
 
 	@Autowired
@@ -16,38 +20,48 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-/*		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요?");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
- 		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);  // 첫번째 질문 저장
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);  // 두번째 질문 저장 */
-//		List<Question> all = this.questionRepository.findAll();
-//		assertEquals(2, all.size());
+//		Question q1 = new Question();
+//		q1.setSubject("sbb가 무엇인가요?");
+//		q1.setContent("sbb에 대해서 알고 싶습니다.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);  // 첫번째 질문 저장
 //
-//		Question q = all.get(0);
-//		assertEquals("sbb가 무엇인가요?", q.getSubject());
-//
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		if(oq.isPresent()) {
-//			Question q = oq.get();
-//			assertEquals("sbb가 무엇인가요?", q.getSubject());
-//		}
-//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-//		assertEquals(1, q.getId());
-//		Question q = this.questionRepository.findBySubjectAndContent(
-//				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-//		assertEquals(1, q.getId());
+//		Question q2 = new Question();
+//		q2.setSubject("스프링부트 모델 질문입니다.");
+//		q2.setContent("id는 자동으로 생성되나요?");
+//		q2.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q2);  // 두번째 질문 저장
 
+//        List<Question> all = this.questionRepository.findAll();
+//        assertEquals(2, all.size());
+//
+//        Question q = all.get(0);
+//        assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        if(oq.isPresent()) {
+//            Question q = oq.get();
+//            assertEquals("sbb가 무엇인가요?", q.getSubject());
+//        }
+
+//        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//        assertEquals(1, q.getId());
+
+//        Question q = this.questionRepository.findBySubjectAndContent(
+//                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//        assertEquals(1, q.getId());
+
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        q.setSubject("수정된 제목");
+//        this.questionRepository.save(q);
+
+		assertEquals(2, this.questionRepository.count());
 		Optional<Question> oq = this.questionRepository.findById(1);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
-		q.setSubject("수정된 제목");
-		this.questionRepository.save(q);
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
 	}
 }
