f9f83ba2168306a1cbedaa4b1eefed1c87885172
jiyoung-OyO <kimwldud1234@gmail.com>
2024-05-10 14:13:05 +0900

    유효성 체크 수동

--- src/main/java/com/sbs/sbb/MainController.java
+++ src/main/java/com/sbs/sbb/MainController.java
@@ -2,15 +2,14 @@ package com.sbs.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/sbb")
-    @ResponseBody
     public String index() {
-        return "안녕하세요!!!!";
+        return "list";
     }
+
     @GetMapping("/")
     public String root() {
         return "redirect:/question/list";
