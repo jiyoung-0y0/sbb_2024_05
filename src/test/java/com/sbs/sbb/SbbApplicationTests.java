6c7cda7966b6abe8943c1a5dc00c18c4c9bb856c
jiyoung-OyO <kimwldud1234@gmail.com>
2024-05-13 18:11:17 +0900

    로그인 기능 구현

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -50,7 +50,9 @@ class SbbApplicationTests {
 
 		// 모든 데이터 삭제
 		// 흔적 삭제 -> 다음번 INSERT를 할 때 id가 1번으로 설정되도록
+		userRepository.deleteAll();
 		userRepository.clearAutoIncrement();
+
 
 		// 회원 2명 생성
 		userService.create("user1", "user1@test.com", "1234");
